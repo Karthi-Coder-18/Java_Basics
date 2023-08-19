5c9597a2bc89ab8b75ba1e907456ce2aa9cf3d73
karthi <karthireddy18@gmail.com>
2023-08-19 11:23:48 +0530

    Practiced some excersies related to loops , objects & Conditions in java

---
+++ Java/src/practice/objects/DrumKit.java
@@ -0,0 +1,22 @@
+package practice.objects;
+
+public class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+
+    public void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    public void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+
+
+
+
+
+}
---
+++ Java/src/practice/objects/Echo.java
@@ -0,0 +1,11 @@
+package practice.objects;
+
+public class Echo {
+    int count = 0;
+
+    public void hello() {
+        System.out.println(" Hello!!....");
+    }
+
+
+}

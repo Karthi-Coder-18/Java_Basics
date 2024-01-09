4b18bf78c943cafa132e74ce6a6bbec00b11ef35
karthi <karthireddy18@gmail.com>
2024-01-09 11:22:25 +0530

    January 9th 2024
    -> Throws keyword
    -> Custom exception in java

--- Java/src/exceptions_in_java/MultipleCatchBlocks.java
+++ Java/src/exceptions_in_java/MultipleCatchBlocks.java
@@ -1,26 +1,57 @@
 package exceptions_in_java;
 
+class KarthikException extends Exception {
+    // Create an constructor for the class
+
+    public KarthikException(String message) {
+        super(message);
+    }
+}
+
 public class MultipleCatchBlocks {
 
     public static void main(String[] args) {
 
-        String[] names = { "karthi", "Surya", "Chiru" };
+    /*     // String[] names = { "karthi", "Surya", "Chiru" };
+
+        double k = 2034.0202;
+
+        int d = (int) k; //
+
         int i = 0;
 
+        int j;
+
         try {
-            int j = 10 / i;
-            System.out.println(j);
-            System.out.println(names[4]); // when the exception occurs in one statement and then why does JVM does not go to next statement ? 
-            
-        } catch (ArithmeticException e) {
-            System.out.println("Do not check for value out of array reach");
-        } catch(Exception e){
-            System.out.println("Exception class will catch this exception: " + e);
+            j = 18 / i;
+            if (j == 0)
+                throw new KarthikException("My custom exception");
+        } catch (KarthikException exception) {
+            j = 19 / i;
+            System.out.println("You cannot divide a number with zero!!" + exception);
+        } /* catch (Exception e) {
+            System.out.println("ooo");
+        } 
+        // System.out.println(j);
+        System.out.println(d); */
+
+
+        int i = 0;
+
+        int j;
+
+        try{
+            j = 18/i;
+            if(j==0)
+            throw new KarthikException("Custom Exception");
+        }catch(KarthikException e){
+            j = 18/1;
+            System.out.println("Karthi ");
         }
 
     }
 }
 
-
-// Always place the Exception class Catch block at the end of other catch blocks so that if there is any other unwanted exception occurs then the Exception class will catch the error. 
-
+// Always place the Exception class Catch block at the end of other catch blocks
+// so that if there is any other unwanted exception occurs then the Exception
+// class will catch the error.
